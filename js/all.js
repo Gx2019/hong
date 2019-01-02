b08f8da9d38b0f96635b8aacc736688e13f241c8
Gx2019 <2572123017@qq.com>
2019-01-02 20:55:16 +0800

    first

---
+++ js/all.js
@@ -0,0 +1,23 @@
+$(function () {
+		$('.left-box ul li a').on('click', function (ev) {
+			ev.preventDefault();
+			var top = $('#' + $(this).attr('title')).offset().top;
+			$(this).addClass("active");
+			// $(".active").removeAttr("background");
+			// .find("span").removeClass("active");
+			// $("active").removeClass("span");
+			$('body,html').animate({
+				scrollTop: top,
+			}, 1000);
+
+
+		});
+		$(window).scroll(function () {//滚动浏览器就会执行
+			var _top = $(window).scrollTop();
+			if (_top > 480 && _top<3800) {
+				$('.link').show();
+			} else {
+				$('.link').hide();
+			};
+		});
+	});
