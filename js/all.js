3a850cc94e08fbf257acaaff03524759c5f3623d
Gx2019 <2572123017@qq.com>
2019-01-03 08:41:00 +0800

    first

--- js/all.js
+++ js/all.js
@@ -2,14 +2,14 @@ $(function () {
 		$('.left-box ul li a').on('click', function (ev) {
 			ev.preventDefault();
 			var top = $('#' + $(this).attr('title')).offset().top;
-			$(this).addClass("active");
+			$(this).addClass('active').siblings().removeClass('active');
+			// $(this).addClass("active")
 			// $(".active").removeAttr("background");
 			// .find("span").removeClass("active");
 			// $("active").removeClass("span");
 			$('body,html').animate({
 				scrollTop: top,
 			}, 1000);
-
 
 		});
 		$(window).scroll(function () {//滚动浏览器就会执行
