d4139804fa551476eadfc91a1abf670d43c5d955
Gx2019 <46307097+Gx2019@users.noreply.github.com>
2019-01-07 14:02:56 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -1,32 +1,29 @@
-$(function() {
-    $('.left-box ul li a').on('click', function(ev) {
-        ev.preventDefault();
-        var top = $('#' + $(this).attr('title')).offset().top;
-        $(this).addClass('active').parent().siblings().children('a').removeClass('active');
-        // $(this).addClass("active")
-        // $(".active").removeAttr("background");
-        // .find("span").removeClass("active");
-        // $("active").removeClass("span");
-        $('body,html').animate({
-            scrollTop: top,
-        }, 1000);
+$(function () {
+		$('.left-box ul li a').on('click', function (ev) {
+			ev.preventDefault();
+			var top = $('#' + $(this).attr('title')).offset().top;
+			$(this).addClass('active').parent().siblings().children('a').removeClass('active');
+			$('body,html').animate({
+				scrollTop: top,
+			}, 1000);
 
-    });
-    $(window).scroll(function() {
-        var _top = $(window).scrollTop();
-        if (_top > 480 && _top < 3800) {
-            $('.link').show();
-        } else {
-            $('.link').hide();
-        };
-    }); //长虹楼梯效果
+		});
+		$(window).scroll(function () {//滚动浏览器就会执行
+			var _top = $(window).scrollTop();
+			if (_top > 480 && _top<3800) {
+				$('.link').show();
+			} else {
+				$('.link').hide();
+			};
 
+		});
 
-
-
-
-
+		$("img.lazy").lazyload({
+			effect: "fadeIn",
+			placeholder:"./img/loading.gif",
+			// threshold: 200
+		});
+		
 
 
-
-});
+	});
